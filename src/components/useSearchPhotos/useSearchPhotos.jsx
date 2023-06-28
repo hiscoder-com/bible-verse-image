df9de2be3b6bffe28478c04bcba632aadd28e902
DenisArger <den.arger@gmail.com>
2023-06-28 16:05:21 +0300

    feat: userSearchPhotos.js

---
+++ src/components/useSearchPhotos/useSearchPhotos.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+function useSearchPhotos(queryObject) {
+  return <></>;
+}
+
+useSearchPhotos.defaultProps = {
+  queryObject: [],
+};
+
+useSearchPhotos.propTypes = {
+  /** queryObject */
+  queryObject: PropTypes.object,
+};
+
+export default useSearchPhotos;
