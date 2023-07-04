b86297f3da6814139f5bead8efe44e44edc4d2c2
Denis Arger <den.arger@gmail.com>
2023-07-04 08:57:39 +0300

    fix: dotenv replace dotenv-webpack

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,16 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 const { name, version, url } = require('./package.json');
-const dotenv = require('dotenv');
-
-// call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
-
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 
 let sections = [
   {
@@ -126,6 +117,6 @@ module.exports = {
         { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
       ],
     },
-    plugins: [new webpack.DefinePlugin(envKeys)],
+    plugins: [new Dotenv()],
   },
 };
