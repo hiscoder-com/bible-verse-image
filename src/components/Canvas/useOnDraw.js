412747fd0d9c8274ff312662ecadc0bbb37f04f9
Denis Arger <den.arger@gmail.com>
2023-07-05 12:02:11 +0300

    feat: rename custom hook file

--- src/components/Canvas/useOnDraw.js
+++ src/components/Canvas/useOnDraw.js
@@ -2,16 +2,26 @@ import { useEffect, useRef } from 'react';
 
 export function useOnDraw() {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  function setCanvasRef(ref) {
+    canvasRef.current = ref;
+    if (ref) {
+      contextRef.current = ref.getContext('2d');
+    }
+  }
 
   useEffect(() => {
     if (canvasRef.current) {
-      // Размеры холста
       canvasRef.current.height = 1200;
       canvasRef.current.width = 1200;
+      contextRef.current = canvasRef.current.getContext('2d');
     }
   }, []);
 
   return {
     canvasRef,
+    contextRef,
+    setCanvasRef,
   };
 }
