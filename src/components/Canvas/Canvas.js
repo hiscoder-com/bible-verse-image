7728be3ffbda35484df67b2e7451a15a6b1c5e7b
DenisArger <den.arger@gmail.com>
2023-07-09 16:25:52 +0300

    fix: error file name, add value defaul

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -3,26 +3,27 @@ import { useCanvasContext } from './useCanvasContext';
 
 const Canvas = ({ infoimage, textStyles, ...props }) => {
   const { contextRef, setCanvasRef } = useCanvasContext(
-    infoimage.height,
-    infoimage.width
+    infoimage.height ?? 1200,
+    infoimage.width ?? 1200
   );
 
   const draw = () => {
-    const ctx = contextRef.current;
+    const ctx = contextRef?.current;
     if (!ctx) {
       return;
     }
-    const pic = new Image();
-    pic.src = infoimage.srcimage;
-    pic.onload = function () {
-      const x0 = 0;
-      const y0 = 0;
-      const x1 = infoimage.widthmage;
-      const y1 = infoimage.heightimage;
+    if (infoimage.srcimage) {
+      const pic = new Image();
+      pic.src = infoimage.srcimage;
+      pic.onload = function () {
+        const x0 = 0;
+        const y0 = 0;
+        const x1 = infoimage.widthmage;
+        const y1 = infoimage.heightimage;
 
-      ctx.drawImage(pic, x0, y0, x1, y1);
-    };
-
+        ctx.drawImage(pic, x0, y0, x1, y1);
+      };
+    }
     textStyles.forEach((style) => {
       ctx.fillStyle = style.fillStyle;
       ctx.font = style.font;
