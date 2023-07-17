56e1f533e152e21d9dd8b8d8dbc4d2c296178854
Denis Arger <den.arger@gmail.com>
2023-07-17 13:53:51 +0300

    feat: change text from input text

--- src/components/BibleVerseImage/BibleVerseImage.js
+++ src/components/BibleVerseImage/BibleVerseImage.js
@@ -6,60 +6,14 @@ const infocanvas = {
   width: 1200,
 };
 
-const elements = [
-  {
-    type: 'text',
-    x: 70,
-    y: 70,
-    props: {
-      text: 'OBT',
-      fillStyle: 'white',
-      font: 'small-caps 30px Helvetica, Arial, sans-serif',
-      textAlign: 'center',
-      url: '',
-    },
-  },
-  {
-    type: 'text',
-    x: 600,
-    y: 550,
-    props: {
-      text: 'God is love',
-      fillStyle: 'white',
-      font: 'small-caps 120px Helvetica, Arial, sans-serif',
-      textAlign: 'center',
-      url: '',
-    },
-  },
-
-  {
-    type: 'text',
-    x: 600,
-    y: 650,
-    props: {
-      text: '1 Jon 4:8',
-      fillStyle: 'white',
-      font: 'small-caps 40px Helvetica, Arial, sans-serif',
-      textAlign: 'center',
-    },
-  },
-
-  {
-    type: 'text',
-    x: 600,
-    y: 1150,
-    props: {
-      text: 'ESV',
-      fillStyle: 'white',
-      font: 'small-caps 120px Helvetica, Arial, sans-serif',
-      textAlign: 'center',
-    },
-  },
-];
+const styleClassWidject = {
+  labelStyle: 'text-5xl mb-4',
+  inputStyle: 'text-3xl px-2 leading-10 rounded-2xl bg-white mb-4 border border-black',
+};
 
 const styleClassComponets = {
   labelStyle: 'text-5xl mb-4',
-  inputStyle: 'text-3xl px-2 leading-10 rounded-2xl bg-white mb-4',
+  inputStyle: 'text-3xl px-2 leading-10 rounded-2xl bg-white mb-4 border  border-black',
   buttonStyle:
     'bg-slate-950 text-white pr-6 pl-6  pb-3 pt-3 border-black border-solid rounded-3xl text-2xl cursor-pointer transition hover:bg-stone-900',
   cardStyle: 'mb-4 flex w-6/12 h-3/6',
@@ -68,14 +22,65 @@ const styleClassComponets = {
   cardLisStyle: 'columns-3',
 };
 
-export default function BibleVerseImage() {
-  const [url, setUrl] = useState('');
+const BibleVerseImage = () => {
+  const [url, setUrl] = useState(
+    'https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjQwOTl8MHwxfHNlYXJjaHwxfHx3aGl0ZXxlbnwwfHx8fDE2ODczNDczNTZ8MA&ixlib=rb-4.0.3&q=80&w=1200'
+  );
   const [infoimage, setInfoImage] = useState({
     srcimage: url,
-    // zoom: 1,
-    // offsetX: 100,
-    // offsetY: 0,
   });
+  const [elements, setElements] = useState([
+    {
+      type: 'text',
+      textWidget: 'Company: ',
+      x: 70,
+      y: 70,
+      props: {
+        text: 'OBT',
+        fillStyle: 'black',
+        font: 'small-caps 30px Helvetica, Arial, sans-serif',
+        textAlign: 'center',
+        url: '',
+      },
+    },
+    {
+      type: 'text',
+      textWidget: 'Verse: ',
+      x: 600,
+      y: 550,
+      props: {
+        text: 'God is love',
+        fillStyle: 'black',
+        font: 'small-caps 120px Helvetica, Arial, sans-serif',
+        textAlign: 'center',
+        url: '',
+      },
+    },
+    {
+      type: 'text',
+      textWidget: 'Ref: ',
+      x: 600,
+      y: 650,
+      props: {
+        text: '1 Jon 4:8',
+        fillStyle: 'black',
+        font: 'small-caps 40px Helvetica, Arial, sans-serif',
+        textAlign: 'center',
+      },
+    },
+    {
+      type: 'text',
+      textWidget: 'Src: ',
+      x: 600,
+      y: 1150,
+      props: {
+        text: 'ESV',
+        fillStyle: 'black',
+        font: 'small-caps 120px Helvetica, Arial, sans-serif',
+        textAlign: 'center',
+      },
+    },
+  ]);
 
   const handleImageChange = (imageUrl) => {
     setUrl(imageUrl);
@@ -85,9 +90,18 @@ export default function BibleVerseImage() {
     }));
   };
 
+  const handleTextChange = (index, event) => {
+    const { value } = event.target;
+    setElements((prevElements) => {
+      const updatedElements = [...prevElements];
+      updatedElements[index].props.text = value;
+      return updatedElements;
+    });
+  };
+
   return (
     <div>
-      <div>
+      <div className="border  border-black">
         <SearchPhotosApp
           handleChangeUrl={handleImageChange}
           styleClassComponets={styleClassComponets}
@@ -96,16 +110,18 @@ export default function BibleVerseImage() {
       <br />
       <br />
       <br />
-      <div>
+      <div className="border  border-black">
         {elements.map((textStyle, index) => (
           <div key={index}>
-            <label htmlFor="text">{textStyle.props.text}</label>
+            <label className={styleClassWidject.labelStyle} htmlFor="text">
+              {textStyle.textWidget}
+            </label>
             <input
-              className={styleClassComponets.inputStyle}
+              className={styleClassWidject.inputStyle}
               type="text"
               name="text"
               value={textStyle.props.text}
-              readOnly
+              onChange={(event) => handleTextChange(index, event)}
             />
           </div>
         ))}
@@ -124,4 +140,6 @@ export default function BibleVerseImage() {
       </div>
     </div>
   );
-}
+};
+
+export default BibleVerseImage;
