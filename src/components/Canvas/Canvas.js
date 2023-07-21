4755f635bd0c21cb04d5a2473395ec2b326372ec
Denis Arger <den.arger@gmail.com>
2023-07-21 13:20:27 +0300

    fix: solved the problem when drawing the background and elements

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCanvasContext } from './useCanvasContext';
 
 const Canvas = ({ infocanvas, infoimage, elements, ...props }) => {
@@ -20,21 +20,24 @@ const Canvas = ({ infocanvas, infoimage, elements, ...props }) => {
     const pic = new Image();
     pic.src = imageSource;
 
-    pic.onload = function () {
-      const { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height } =
-        calculateImageParameters(pic, ctx, infoimage);
-      ctx.drawImage(
-        pic,
-        sourceX,
-        sourceY,
-        sourceWidth,
-        sourceHeight,
-        x,
-        y,
-        width,
-        height
-      );
-    };
+    return new Promise((resolve) => {
+      pic.onload = function () {
+        const { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height } =
+          calculateImageParameters(pic, ctx, infoimage);
+        ctx.drawImage(
+          pic,
+          sourceX,
+          sourceY,
+          sourceWidth,
+          sourceHeight,
+          x,
+          y,
+          width,
+          height
+        );
+        resolve();
+      };
+    });
   };
 
   const drawImage = (ctx, style) => {
@@ -88,132 +91,136 @@ const Canvas = ({ infocanvas, infoimage, elements, ...props }) => {
       }
     });
   };
-  const draw = () => {
+
+  const calculateImageParameters = (pic, ctx, infoimage) => {
+    let sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height;
+
+    sourceX = 0;
+    sourceY = 0;
+    sourceWidth = pic.width;
+    sourceHeight = pic.height;
+    x = 0;
+    y = 0;
+    width = ctx.canvas.width;
+    height = ctx.canvas.height;
+
+    if (infoimage.zoom === undefined) {
+      ({ sourceX, sourceY, sourceWidth, sourceHeight } = centerImage(
+        pic,
+        ctx.canvas.width,
+        ctx.canvas.height
+      ));
+    } else {
+      const zoom = infoimage.zoom;
+      width = pic.width * zoom;
+      height = pic.height * zoom;
+      x = (ctx.canvas.width - width) / 2;
+      y = (ctx.canvas.height - height) / 2;
+    }
+
+    if (infoimage.offsetX !== undefined && infoimage.offsetY !== undefined) {
+      x += infoimage.offsetX;
+      y += infoimage.offsetY;
+    }
+
+    return { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height };
+  };
+
+  const centerImage = (pic, canvasWidth, canvasHeight) => {
+    const imageWidth = pic.width;
+    const imageHeight = pic.height;
+
+    const canvasAspectRatio = canvasWidth / canvasHeight;
+    const imageAspectRatio = imageWidth / imageHeight;
+
+    let sourceX, sourceY, sourceWidth, sourceHeight;
+
+    if (imageAspectRatio > canvasAspectRatio) {
+      sourceWidth = imageHeight * canvasAspectRatio;
+      sourceHeight = imageHeight;
+      sourceX = (imageWidth - sourceWidth) / 2;
+      sourceY = 0;
+    } else {
+      sourceWidth = imageWidth;
+      sourceHeight = imageWidth / canvasAspectRatio;
+      sourceX = 0;
+      sourceY = (imageHeight - sourceHeight) / 2;
+    }
+
+    return { sourceX, sourceY, sourceWidth, sourceHeight };
+  };
+
+  const drawWrappedText = (
+    ctx,
+    text,
+    x,
+    y,
+    blockWidth,
+    lineHeight,
+    fontHeight,
+    alignment
+  ) => {
+    const words = text.split(' ');
+    let line = '';
+    blockWidth = blockWidth ?? ctx.canvas.width;
+
+    for (let i = 0; i < words.length; i++) {
+      let testLine = line + words[i];
+      const metrics = ctx.measureText(testLine);
+      const testWidth = metrics.width;
+      testLine += ' ';
+
+      if (testWidth > blockWidth) {
+        let offsetX = 0;
+
+        switch (alignment) {
+          case 'center':
+            offsetX = (blockWidth - ctx.measureText(line).width) / 2;
+            break;
+          case 'right':
+            offsetX = blockWidth - ctx.measureText(line).width;
+            break;
+          default:
+            break;
+        }
+
+        ctx.fillText(line, x + offsetX, y + fontHeight);
+        line = words[i] + ' ';
+        y += lineHeight;
+      } else {
+        line = testLine;
+      }
+    }
+
+    let offsetX = 0;
+
+    switch (alignment) {
+      case 'center':
+        offsetX = (blockWidth - ctx.measureText(line).width) / 2;
+        break;
+      case 'right':
+        offsetX = blockWidth - ctx.measureText(line).width;
+        break;
+      default:
+        break;
+    }
+    ctx.fillText(line, x + offsetX, y + lineHeight);
+  };
+  const draw = async () => {
     console.log('Drawing...');
     const ctx = contextRef?.current;
     if (!ctx) {
       return;
     }
 
-    const calculateImageParameters = (pic, ctx, infoimage) => {
-      let sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height;
-
-      sourceX = 0;
-      sourceY = 0;
-      sourceWidth = pic.width;
-      sourceHeight = pic.height;
-      x = 0;
-      y = 0;
-      width = ctx.canvas.width;
-      height = ctx.canvas.height;
-
-      if (infoimage.zoom === undefined) {
-        ({ sourceX, sourceY, sourceWidth, sourceHeight } = centerImage(
-          pic,
-          ctx.canvas.width,
-          ctx.canvas.height
-        ));
-      } else {
-        const zoom = infoimage.zoom;
-        width = pic.width * zoom;
-        height = pic.height * zoom;
-        x = (ctx.canvas.width - width) / 2;
-        y = (ctx.canvas.height - height) / 2;
-      }
-
-      if (infoimage.offsetX !== undefined && infoimage.offsetY !== undefined) {
-        x += infoimage.offsetX;
-        y += infoimage.offsetY;
-      }
-
-      return { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height };
-    };
-
-    const centerImage = (pic, canvasWidth, canvasHeight) => {
-      const imageWidth = pic.width;
-      const imageHeight = pic.height;
-
-      const canvasAspectRatio = canvasWidth / canvasHeight;
-      const imageAspectRatio = imageWidth / imageHeight;
-
-      let sourceX, sourceY, sourceWidth, sourceHeight;
-
-      if (imageAspectRatio > canvasAspectRatio) {
-        sourceWidth = imageHeight * canvasAspectRatio;
-        sourceHeight = imageHeight;
-        sourceX = (imageWidth - sourceWidth) / 2;
-        sourceY = 0;
-      } else {
-        sourceWidth = imageWidth;
-        sourceHeight = imageWidth / canvasAspectRatio;
-        sourceX = 0;
-        sourceY = (imageHeight - sourceHeight) / 2;
-      }
-
-      return { sourceX, sourceY, sourceWidth, sourceHeight };
-    };
-
-    const drawWrappedText = (
-      ctx,
-      text,
-      x,
-      y,
-      blockWidth,
-      lineHeight,
-      fontHeight,
-      alignment
-    ) => {
-      const words = text.split(' ');
-      let line = '';
-      blockWidth = blockWidth ?? ctx.canvas.width;
-
-      for (let i = 0; i < words.length; i++) {
-        let testLine = line + words[i];
-        const metrics = ctx.measureText(testLine);
-        const testWidth = metrics.width;
-        testLine += ' ';
-
-        if (testWidth > blockWidth) {
-          let offsetX = 0;
-
-          switch (alignment) {
-            case 'center':
-              offsetX = (blockWidth - ctx.measureText(line).width) / 2;
-              break;
-            case 'right':
-              offsetX = blockWidth - ctx.measureText(line).width;
-              break;
-            default:
-              break;
-          }
-
-          ctx.fillText(line, x + offsetX, y + fontHeight);
-          line = words[i] + ' ';
-          y += lineHeight;
-        } else {
-          line = testLine;
-        }
-      }
-
-      let offsetX = 0;
-
-      switch (alignment) {
-        case 'center':
-          offsetX = (blockWidth - ctx.measureText(line).width) / 2;
-          break;
-        case 'right':
-          offsetX = blockWidth - ctx.measureText(line).width;
-          break;
-        default:
-          break;
-      }
-      ctx.fillText(line, x + offsetX, y + lineHeight);
-    };
-
-    // if (infoimage.srcimage) {
-    //   drawImageOnCanvas(ctx, infoimage.srcimage, infoimage, calculateImageParameters);
-    // }
-
+    if (infoimage.srcimage) {
+      await drawImageOnCanvas(
+        ctx,
+        infoimage.srcimage,
+        infoimage,
+        calculateImageParameters
+      );
+    }
     drawElementsOnCanvas(
       ctx,
       elements.filter((style) => style.type === 'image'),
@@ -230,7 +237,6 @@ const Canvas = ({ infocanvas, infoimage, elements, ...props }) => {
   useEffect(() => {
     draw();
   }, [draw]);
-
   return <canvas ref={setCanvasRef} {...props} />;
 };
 
