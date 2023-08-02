67dc2d3a75cb3dd487795797e256bd65159fc01e
DenisArger <den.arger@gmail.com>
2023-08-02 17:37:02 +0300

    fix: delete backgroundimage from param

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useCanvasContext } from './useCanvasContext';
 import { drawElementsOnCanvas } from './canvasUtils';
 
-const Canvas = ({ infocanvas, backgroundimage, elements, ...props }) => {
+const Canvas = ({ infocanvas, elements, ...props }) => {
   const { contextRef, setCanvasRef } = useCanvasContext(
     infocanvas.height ?? 1200,
     infocanvas.width ?? 1200
@@ -19,7 +19,7 @@ const Canvas = ({ infocanvas, backgroundimage, elements, ...props }) => {
     };
 
     draw();
-  }, [backgroundimage, elements]);
+  }, [elements]);
 
   return <canvas ref={setCanvasRef} {...props} />;
 };
