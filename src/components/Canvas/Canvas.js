cb52ef646900614e61ae10a795d8c342477751cf
DenisArger <den.arger@gmail.com>
2023-08-03 16:11:55 +0300

    fix: fixed the use of rect.memo

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, memo } from 'react';
 import { useCanvasContext } from './useCanvasContext';
 import { drawElementsOnCanvas } from './canvasUtils';
 
 // eslint-disable-next-line react/display-name
-const Canvas = React.memo(({ infocanvas, elements, ...props }) => {
+const Canvas = memo(({ infocanvas, elements, ...props }) => {
   const { contextRef, setCanvasRef } = useCanvasContext(
     infocanvas.height ?? 1200,
     infocanvas.width ?? 1200
